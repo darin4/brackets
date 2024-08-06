31b561366419c5c361774f0995b10f82cfbd121d
darin4 <zvetochek23@mail.ru>
2024-08-07 00:45:38 +0300

    docs: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let stack = [];
+let openBrackets = [];
+let closeBrackets = [];
+let sameBrackets = new Set();
+
+for (let i = 0; i < bracketsConfig.length; i++) {
+  let config = bracketsConfig[i];
+  openBrackets.push(config[0]);
+  closeBrackets.push(config[1]);
+  if (config[0] === config[1]) {
+    sameBrackets.add(config[0]);
+  }
+}
+
+for (let i = 0; i < str.length; i++) {
+  let bracket = str[i];
+  if (openBrackets.includes(bracket)) {
+    if (sameBrackets.has(bracket) && stack[stack.length - 1] === bracket) {  
+      stack.pop();
+    }
+     else {
+      stack.push(bracket);
+    }
+  } else if (closeBrackets.includes(bracket)) {
+    let index = closeBrackets.indexOf(bracket);
+    if (stack.length === 0 || openBrackets[index] !==stack.pop()) {
+      return false;
+    }
+    }  
+}
+
+return stack.length === 0;
+
 }
